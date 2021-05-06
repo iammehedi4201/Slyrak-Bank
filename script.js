d2bae49db399955e30f07cf4bc8a1d5f8dc5c163
Mehedi <iammehedi296@gmail.com>
2021-05-06 23:08:34 +0600

    I almost finished the styling section

---
+++ script.js
@@ -0,0 +1,13 @@
+let button= document.getElementById("loginBtn")
+
+button.addEventListener("click",function() {
+    
+ let loginArea= document.getElementById("loginArea");
+
+ loginArea.style.display="none";
+
+ let transactionArea = document.getElementById("tracsaction-Area");
+
+ transactionArea.style.display="block";
+  
+})
